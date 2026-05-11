f09f1cea423a0b9d91c2aa1acdb7ec679fcd7cd1
MGG <migargar04@gmail.com>
2026-05-11 16:24:34 +0200

    Modificaciones para arreglar valoraciones

--- src/main/java/Conexiones/service/ValoracionService.java
+++ src/main/java/Conexiones/service/ValoracionService.java
@@ -1,25 +1,44 @@
 package Conexiones.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import Conexiones.model.Producto;
+import Conexiones.model.Usuario;
 import Conexiones.model.Valoracion;
+import Conexiones.repository.ProductoRepository;
+import Conexiones.repository.UsuarioRepository;
 import Conexiones.repository.ValoracionRepository;
 
 @Service
 public class ValoracionService {
 
-	private final ValoracionRepository valoracionRepository;
-
-	public ValoracionService(ValoracionRepository valoracionRepository) {
-		super();
-		this.valoracionRepository = valoracionRepository;
-	}
+	@Autowired
+	private ValoracionRepository valoracionRepository;
+	@Autowired
+	private ProductoRepository productoRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public Valoracion crear(Valoracion valoracion) {
-		return valoracionRepository.save(valoracion);
-	}
+
+        Long idProducto = valoracion.getProducto().getIdProducto();
+        Long idUsuario = valoracion.getUsuario().getId();
+
+        Producto producto = productoRepository.findById(idProducto)
+                .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+
+        Usuario usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        valoracion.setProducto(producto);
+        valoracion.setUsuario(usuario);
+
+        
+        return valoracionRepository.save(valoracion);
+    }
 
 	public List<Valoracion> obtenerPorProducto(Long productoId) {
 		return valoracionRepository.findByProductoId(productoId);
